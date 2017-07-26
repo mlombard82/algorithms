ab41a340d84b446c50467d0def2276adbb81af9e
mlombard82 <mlombard82@gmail.com>
2017-07-26 11:44:13 -0500

    reverses the characters in a message

--- 27_reverse_words.js
+++ 27_reverse_words.js
@@ -4,7 +4,8 @@ and reverses the order of the words in place
 */
 
 /* Ex of 2 functions that do the same thing. One is in-place
-the other one is out of place*/
+the other one is out of place. Generally, out-of-place Alg are
+considered safer because they AVOID SIDE EFFECTS.*/
 
 function squareArrayInPlace (intArray){
     intArray.forEach(function(int, index){
@@ -24,3 +25,21 @@ function squareArrayOutOfPlace(intArray){
 
     return squaredArray;
 };
+
+
+/*Let's start reversing all characters in the message */
+
+function reverseCharacters(message){
+    var messageArray = message.split('');
+
+    // walk towards the middle from both sides
+    for(var frontIndex = 0; frontIndex < messageArray.length/2; frontIndex++){
+        var backIndex = messageArray.length - 1 - frontIndex;
+        //swap the front char and back char
+        var tempChar = messageArray[frontIndex];
+        messageArray[frontIndex] = messageArray[backIndex];
+        messageArray[backIndex]= tempChar;
+    }
+
+    return messageArray.join('');
+}
