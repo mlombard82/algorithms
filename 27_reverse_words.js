3371154a8b13d40b2e022f859efeff8dae611b8a
mlombard82 <mlombard82@gmail.com>
2017-07-26 13:12:12 -0500

    adds solution

--- 27_reverse_words.js
+++ 27_reverse_words.js
@@ -43,3 +43,48 @@ function reverseCharacters(message){
 
     return messageArray.join('');
 }
+
+//SOLUTION. We'll first write a helper function
+
+function reverseWords(message){
+    var messageArray = message.split('');
+
+    //first we reverse all char in the entire messageArray
+    reverseCharacters(messageArray, 0, messageArray.length - 1);
+    
+    // this gives us the right word order
+    // but with each word backwards
+
+    //now we'll make the words forward again
+    //by reversing each word's characters
+
+    //we hold the index of the *start* of the current word
+    //as we look for the *end* of the current word
+    var currentWordStartIndex = 0;
+    for(var i = 0; i <= messageArray.length; i++){
+
+        //found the end of the current word!
+        if(i === messageArray.length || messageArray[i] === ' '){
+
+            //if we haven't exhausted the string our next word's 
+            //start is one character ahead
+            reverseCharacters(messageArray, currentWordStartIndex, i - 1);
+            currentWordStartIndex = i + 1;
+        }
+    }
+
+    return messageArray.join('');
+}
+
+function reverseCharacters(messageArray, startIndex, endIndex){
+    //walk towards the middle, from both sides
+    while(startIndex < endIndex){
+        //swap the front char and back char
+        var temp = messageArray[startIndex];
+        messageArray[startIndex] = messageArray[endIndex];
+        messageArray[endIndex] = temp;
+        startIndex++;
+        endIndex--;
+    }
+}
+
